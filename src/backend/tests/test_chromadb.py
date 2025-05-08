bbf705d7e1262e57a76972c44232670f8ce4d7f9
Yuyang Lou <evanlou795@gmail.com>
2025-05-08 15:04:22 -0700

    integrated db with processing pipeline

--- src/backend/tests/test_chromadb.py
+++ src/backend/tests/test_chromadb.py
@@ -1,5 +1,7 @@
 import pytest
-from database.chroma_client import MetadataDict, add_document, get_document_by_id, find_similar_documents
+from database.chroma_client import MetadataDict, ChromaClient
+import tempfile
+import chromadb
 
 @pytest.fixture
 def test_metadata():
@@ -15,6 +17,13 @@ def test_metadata():
 def test_document():
     return "This is the full document string."
 
+@pytest.fixture
+def test_client():
+    tempdir = tempfile.mkdtemp()
+    
+    return ChromaClient(db_dir=tempdir, collection_name="dummy_collection")
+
+
 def test_metadata_to_dict(test_metadata):
     result = test_metadata.to_dict()
     assert result["id"] == "test123"
@@ -23,31 +32,27 @@ def test_metadata_to_dict(test_metadata):
     assert result["source_title"] == "Test Title"
     assert result["source_author"] == "Test Author"
 
-def test_add_and_get_document(test_metadata, test_document):
-    doc_id = add_document(test_document, test_metadata.to_dict())
-    assert doc_id == test_metadata.id
+def test_add_and_get_document(test_metadata, test_document, test_client):
+    document = test_client.add_document(test_document, [test_metadata.to_dict()])
+    assert document == test_document
 
-    result = get_document_by_id(doc_id)
+    result = test_client.get_document_by_id(test_metadata.id)
+    print(result["ids"][0])
     assert result["ids"][0] == test_metadata.id
     assert result["documents"][0] == test_document
     assert result["metadatas"][0]["source_title"] == test_metadata.source_title
     assert result["metadatas"][0]["source_author"] == test_metadata.source_author
     assert result["metadatas"][0]["text_chunk"] == test_metadata.text_chunk
     
-def test_find_similar_documents(test_metadata, test_document):
+def test_find_similar_documents(test_metadata, test_document, test_client):
     # Ensure the document is in the DB
-    id = add_document(test_document, test_metadata.to_dict())
+    document = test_client.add_document(test_document, [test_metadata.to_dict()])
 
-    results = find_similar_documents(test_metadata.embedding, n_results=1)
+    results = test_client.find_similar_documents(test_metadata.embedding, n_results=1)
     assert "documents" in results
     assert len(results["documents"][0]) >= 1
-    assert id == test_metadata.id
+    assert document == test_document
     assert results["metadatas"][0][0]["source_title"] == test_metadata.source_title
     assert results["metadatas"][0][0]["source_author"] == test_metadata.source_author
     assert results["metadatas"][0][0]["text_chunk"] == test_metadata.text_chunk
-    assert results["metadatas"][0][0]["id"] == test_metadata.id
-
-if __name__ == "__main__":
-    # run tests
-    
-    pytest.main([__file__])
+    assert results["ids"][0][0] == test_metadata.id
